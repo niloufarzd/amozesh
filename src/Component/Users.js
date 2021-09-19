61150a281b89c9c2bed1317681c93df063be441a
niloufar <zaferaniniloufar@gmail.com>
2021-09-19 10:18:18 +0430

    add component spinner

--- src/Component/Users.js
+++ src/Component/Users.js
@@ -1,24 +1,35 @@
-import React, { Component }  from 'react'
+import React  from 'react'
 import UserItem from './UserItem';
+ import  Spinner  from './Spinner';
 
 
 
-class  Users  extends Component {
-render(){
-    return (
-        <div className="userStyle">
-            {this.props.users.map(u =>(
-                <UserItem key={u.id} user={u}></UserItem>
-
-            ))}
-            
-        </div>
-    );
-
-}
+ import PropTypes from 'prop-types';
+ const  Users =({users,loading})=> {
+     if(loading){
+         return <Spinner></Spinner>
+ 
+         
+     }
+     else{
+ 
+         return (
+             <div className="userStyle">
+                 {users.map(u=>(
+                     <UserItem key={u.id} user={u}></UserItem>
+                 ))
+                 }
+             </div>
+         
+         );
+     }
      
-    }
-   
-   
-
-export default Users
+ 
+      
+ }
+ Users.prototype={
+     users:PropTypes.array.isRequired,
+     loading:PropTypes.bool.isRequired
+ }
+ 
+ export default Users;
