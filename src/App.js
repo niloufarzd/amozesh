c56f4d9b6a67fac94cac80eafa131da68a1bfc81
niloufar <zaferaniniloufar@gmail.com>
2021-10-08 19:43:02 +0330

    finished project

--- src/App.js
+++ src/App.js
@@ -1,101 +1,36 @@
-import React, { useState, Fragment } from 'react'
+import React from 'react';
 import './App.css';
-import Navbar from './Component/Navbar';
-import Users from './Component/Users';
-import axios from 'axios';
-import Search from './Component/Search';
-import Alert  from './Component/Alert';
-import About from './Component/Layout/About';
-import User from './Component/User';
- import { BrowserRouter as Router,Route ,Switch} from 'react-router-dom';
-const  App  =()=> {
-   const [users,setUsers]=useState([]);
-   const [user,setUser]=useState({});
-   const [repos,setRepos]=useState([]);
-   const [loading,setLoading]=useState(false);
-   const [alert,setAlert]=useState(null);
+import Navbar from './components/layout/Navbar';
+import User from './components/users/User';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 
-  
-   const search = async text =>{
-    setLoading(true);
-    const res = await axios.get
-    (`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_CLIENT_ID}&client_sectet=${process.env.REACT_APP_CLIENT_SECRET}`
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
-  );
-  setLoading(false);
-  setUsers(res.data.items);
-
-  };
-   const getUser = async username =>{
-    setLoading(true);
-    const res = await axios.get
-    (`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_CLIENT_ID}&client_sectet=${process.env.REACT_APP_CLIENT_SECRET}`
-
-  );
-
-  setLoading(false);
-  setUser(res.data); 
- };
- const getUserRepose = async username =>{
-    setLoading(true);
-
-  const res = await axios.get
-    (`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_CLIENT_ID}&client_sectet=${process.env.REACT_APP_CLIENT_SECRET}`
-
-  );
-  setLoading(false);
-  setRepos(res.data); 
-
-};
-   const clearUser =()=>{
-  setUsers([]);
- setLoading(false);
-
-  };
-   const showAlert =(msg,type)=>{
-      setAlert({message:msg,type:type})
-    setTimeout(() => {
-      setAlert(null)      
-    }, 3000);
-
-  };
-
-    return (
-      <Router>
-      <div className="App">
-      <Navbar>
-      </Navbar>
-      <div className="container">
-        <Alert alert={alert}></Alert>
-        <Switch>
-         <Route exact path='/' render={()=>
-         <Fragment>
-            <Search searchUsers={search}
-         clearUsers={clearUser}
-        showClear={users.length>0} 
-        setAlert={showAlert}></Search>
-         <Users users={users} loading={loading}></Users>
-         </Fragment>
-
-        }></Route>
-        <Route exact path='/about' component={About}></Route>
-        <Route 
-        exact 
-        path='/user/:login'  
-        render={props=>(
-          <User    
-        {...props}
-          getUser={getUser}  getUserRepose={getUserRepose}loading={loading} user={user}  repos={repos}></User>
-        )}></Route>
-
-    
-        </Switch>
+const App = () => {
+  return (
+    <AlertState>
+      <GithubState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
           </div>
-      </div> 
-      </Router>
-    );
-    
-  }
-
-
+        </Router>
+      </GithubState>
+    </AlertState>
+  );
+};
 export default App;
---
+++ src/components/users/a.py
@@ -0,0 +1,8 @@
+# import io, zipfile, StringIO
+try:
+    from StringIO import StringIO ## for Python 2
+except ImportError:
+    print("Nashod")
+    from io import StringIO ## for Python 3
+
+print("Salam")
